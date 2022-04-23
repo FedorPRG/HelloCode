dac3290514b804bb06aa5d8ea10941b3687f54a7
Fedor <fedor_prg@rambler.ru>
2022-04-23 16:47:32 +1000

    Updated Example 012

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -72,9 +72,9 @@ Console.WriteLine(res1);
 
 for (int i = 2; i <= 10; i++)
 {
-    for (int j=2; j<=10; j++)
+    for (int j = 2; j <= 10; j++)
     {
-        Console.WriteLine($"{i} x {j} = {i*j}");
+        Console.WriteLine($"{i} x {j} = {i * j}");
     }
     Console.WriteLine();
 }
@@ -97,19 +97,73 @@ string Replace(string text, char oldValue, char newValue)
 {
     string result = String.Empty;
     int length = text.Length;
-    for (int i=0; i<length; i++)
+    for (int i = 0; i < length; i++)
     {
-        if (text[i]==oldValue) result=result + $"{newValue}";
-        else result=result+$"{text[i]}";
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
     }
     return result;
 }
-string newText=Replace(text, ' ', '|');
+string newText = Replace(text, ' ', '|');
 Console.WriteLine(newText);
 Console.WriteLine();
-newText=Replace(newText, 'к', 'К');
+newText = Replace(newText, 'к', 'К');
 Console.WriteLine(newText);
 Console.WriteLine();
-newText=Replace(newText, 'с', 'С');
+newText = Replace(newText, 'с', 'С');
 Console.WriteLine(newText);
+Console.WriteLine();
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length-1; i++)
+    {
+        int minPosition=i;
+        for (int j=i+1; j<array.Length; j++)
+        {
+            if (array[j]<array[minPosition]) minPosition=j;
+        }
+
+        int temporary=array[i];
+        array[i]=array[minPosition];
+        array[minPosition]=temporary;
+
+    }
+}
+
+void SelectionSortMax(int[] array)
+{
+    for (int i = 0; i < array.Length-1; i++)
+    {
+        int maxPosition=i;
+        for (int j=i+1; j<array.Length; j++)
+        {
+            if (array[j]>array[maxPosition]) maxPosition=j;
+        }
+
+        int temporary=array[i];
+        array[i]=array[maxPosition];
+        array[maxPosition]=temporary;
+
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
+SelectionSortMax(arr);
+PrintArray(arr);
+
 
